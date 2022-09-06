f140422d084eed3a1d65644bd71591066072cbc2
leidy sierra <leidysierra@192.168.0.8>
2022-09-06 17:43:33 -0500

    update hola mundo

---
+++ Intro-GIT/Holamundo.java
@@ -0,0 +1,11 @@
+class Holamundo {
+    public static void main (String [] args){
+        saludar (nombre: "cristhian");
+    }
+
+    public static void saludar (String nombre){
+        System.out.println ("Hola "+nombre"");
+    
+    }
+
+}
