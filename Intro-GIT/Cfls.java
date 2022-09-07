7a5ebbd7ba63015286a3f3c0f8686ae93ed238cc
leidy sierra <leidysierra@192.168.0.8>
2022-09-06 19:21:15 -0500

    fabian

---
+++ Intro-GIT/Cfls.java
@@ -0,0 +1,17 @@
+public class Cfleons {
+  public cfleons (){
+
+    }
+
+  public void conectar (){
+    System.out.println (conectando a la BD...);
+
+    }
+
+
+  public void desconectar (){
+    System.out.println (desconectando a la BD...);
+
+    }
+
+}
