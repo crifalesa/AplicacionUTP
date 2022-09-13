a72747d8e007dcceba627a17cee19f36ce6e222a
leidy sierra <leidysierra@192.168.0.8>
2022-09-13 09:57:43 -0500

    introduccion-js

---
+++ poyecto/js/index.js
@@ -0,0 +1,9 @@
+const users =  [
+    {foto: "", nombre: "cristhian", apellido: "leon", email: "cristin-salas@gmail.es", fecha_de_nacimiento: "10/07/1989" },
+    {foto: "", nombre: "lali", apellido: "leon", email: "lali-salas@gmail.es", fecha_de_nacimiento: "10/06/1969" },
+    {foto: "", nombre: "jeimer", apellido: "leon", email: "Jeimer-salas@gmail.es", fecha_de_nacimiento: "10/04/1942" },
+    {foto: "", nombre: "Karen", apellido: "leon", email: "Karen-salas@gmail.es", fecha_de_nacimiento: "09/03/1992" },
+]
+for (let i = 0; i < users.length; i++){
+    
+}
